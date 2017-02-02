9c68324cea54b7b6fa84961c0ca2c1d59fdbe075
thomasyu888 <thomasyu888@gmail.com>
2017-02-02 02:07:30 -0800

    Start validation script

--- scoring_harness/TESLA_validation.py
+++ scoring_harness/TESLA_validation.py
@@ -1,45 +1,34 @@
 import pandas as pd
 
+def validate_1(submission_filepath, goldstandard_path):
+	assert os.path.basename(submission_filepath) == "TESLA_OUT_1.csv", "Submission file must be named TESLA_OUT_1.csv"
+	required_cols = pd.Series(["VAR_ID","GENE","CHROM","START","END","STRAND","CLASS","REFALLELE","MUTALLELE","REFCOUNT_T","MUTCOUNT_T",
+					 "REFCOUNT_N","MUTDEPTH_N","REF_FPKM_T","REF_HTSEQ_T","MUT_FPKM_T","MUT_HTSEQ_T","QUAL","VARID","INFO"])
+	submission = pd.read_csv(submission_filepath)
+	assert required_cols.isin(submission.columns), "These column headers are missing from your file: %s" % ", ".join(required_cols[~required_cols.isin(submission.columns)])
 
-def validate_1(submission, goldstandard_path):
-    ##Read in submission (submission.filePath)
-    ##Validate submission
-    ## MUST USE ASSERTION ERRORS!!! 
-    ##eg.
-    ## assert os.path.basename(submission.filePath) == "prediction.tsv", "Submission file must be named prediction.tsv"
-    ## or raise AssertionError()...
-    ## Only assertion errors will be returned to participants, all other errors will be returned to the admin
+	assert submission.CHROM.isin(range(1,23) + ["X"]), "CHROM values must be 1-22, or X. You have: %s" % ", ",join(submission.CHROM[~submission.CHROM.isin(range(1,23) + ["X"])]) 
+
     return(True,"Passed Validation")
 
 
-def validate_2(submission, goldstandard_path):
-    ##Read in submission (submission.filePath)
-    ##Validate submission
-    ## MUST USE ASSERTION ERRORS!!! 
-    ##eg.
-    ## assert os.path.basename(submission.filePath) == "prediction.tsv", "Submission file must be named prediction.tsv"
-    ## or raise AssertionError()...
-    ## Only assertion errors will be returned to participants, all other errors will be returned to the admin
+def validate_2(submission_filepath, goldstandard_path):
+	assert os.path.basename(submission_filepath) == "TESLA_OUT_2.csv", "Submission file must be named TESLA_OUT_1.csv"
+	required_cols = pd.Series(["RANK","VAR_ID","PROT_POS","HLA_ALLELE","PEP_LEN","MUT_EPI_SEQ","WT_EPI_SEQ"])
+	submission = pd.read_csv(submission_filepath)
+	assert required_cols.isin(submission.columns), "These column headers are missing from your file: %s" % ", ".join(required_cols[~required_cols.isin(submission.columns)])
     return(True,"Passed Validation")
 
 
-def validate_3(submission, goldstandard_path):
-    ##Read in submission (submission.filePath)
-    ##Validate submission
-    ## MUST USE ASSERTION ERRORS!!! 
-    ##eg.
-    ## assert os.path.basename(submission.filePath) == "prediction.tsv", "Submission file must be named prediction.tsv"
-    ## or raise AssertionError()...
-    ## Only assertion errors will be returned to participants, all other errors will be returned to the admin
+def validate_3(submission_filepath, goldstandard_path):
+	assert os.path.basename(submission_filepath) == "TESLA_OUT_3.csv", "Submission file must be named TESLA_OUT_1.csv"
+	required_cols = pd.Series(["VAR_ID","PROT_POS","HLA_ALLELE","PEP_LEN","MUT_EPI_SEQ","WT_EPI_SEQ","STEP_ID"])
+	submission = pd.read_csv(submission_filepath)
+	assert required_cols.isin(submission.columns), "These column headers are missing from your file: %s" % ", ".join(required_cols[~required_cols.isin(submission.columns)])
+
     return(True,"Passed Validation")
 
-
-def validate_4(submission, goldstandard_path):
-    ##Read in submission (submission.filePath)
-    ##Validate submission
-    ## MUST USE ASSERTION ERRORS!!! 
-    ##eg.
-    ## assert os.path.basename(submission.filePath) == "prediction.tsv", "Submission file must be named prediction.tsv"
-    ## or raise AssertionError()...
-    ## Only assertion errors will be returned to participants, all other errors will be returned to the admin
+#Validate workflow
+def validate_4(submission_filepath, goldstandard_path):
+	assert os.path.basename(submission_filepath) == "TESLA_OUT_4.csv", "Submission file must be named TESLA_OUT_4.csv"
     return(True,"Passed Validation")
