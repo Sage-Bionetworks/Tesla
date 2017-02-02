665d6a7b542141cf37ac78266e6dc75cc19c920f
thomasyu888 <thomasyu888@gmail.com>
2017-02-02 01:50:04 -0800

    Add in scoring harness

---
+++ scoring_harness/TESLA_validation.py
@@ -0,0 +1,45 @@
+import pandas as pd
+
+
+def validate_1(submission, goldstandard_path):
+    ##Read in submission (submission.filePath)
+    ##Validate submission
+    ## MUST USE ASSERTION ERRORS!!! 
+    ##eg.
+    ## assert os.path.basename(submission.filePath) == "prediction.tsv", "Submission file must be named prediction.tsv"
+    ## or raise AssertionError()...
+    ## Only assertion errors will be returned to participants, all other errors will be returned to the admin
+    return(True,"Passed Validation")
+
+
+def validate_2(submission, goldstandard_path):
+    ##Read in submission (submission.filePath)
+    ##Validate submission
+    ## MUST USE ASSERTION ERRORS!!! 
+    ##eg.
+    ## assert os.path.basename(submission.filePath) == "prediction.tsv", "Submission file must be named prediction.tsv"
+    ## or raise AssertionError()...
+    ## Only assertion errors will be returned to participants, all other errors will be returned to the admin
+    return(True,"Passed Validation")
+
+
+def validate_3(submission, goldstandard_path):
+    ##Read in submission (submission.filePath)
+    ##Validate submission
+    ## MUST USE ASSERTION ERRORS!!! 
+    ##eg.
+    ## assert os.path.basename(submission.filePath) == "prediction.tsv", "Submission file must be named prediction.tsv"
+    ## or raise AssertionError()...
+    ## Only assertion errors will be returned to participants, all other errors will be returned to the admin
+    return(True,"Passed Validation")
+
+
+def validate_4(submission, goldstandard_path):
+    ##Read in submission (submission.filePath)
+    ##Validate submission
+    ## MUST USE ASSERTION ERRORS!!! 
+    ##eg.
+    ## assert os.path.basename(submission.filePath) == "prediction.tsv", "Submission file must be named prediction.tsv"
+    ## or raise AssertionError()...
+    ## Only assertion errors will be returned to participants, all other errors will be returned to the admin
+    return(True,"Passed Validation")
