3d04a8ef077b1d4f2a15e34a229f653a1a521b93
thomasyu888 <thomasyu888@gmail.com>
2017-02-08 00:59:46 -0800

    Forumg

--- scoring_harness/messages.py
+++ scoring_harness/messages.py
@@ -17,7 +17,7 @@ dry_run = False
 ## Edit these URLs to point to your challenge and its support forum
 defaults = dict(
     challenge_instructions_url = "https://www.synapse.org/#!Synapse:syn7362874/wiki/407415",
-    support_forum_url = "https://www.synapse.org/#!Synapse:syn7362874/wiki/407415",
+    support_forum_url = "https://www.synapse.org/#!Synapse:syn7362874/discussion/default",
     scoring_script = "The TESLA consortium")
 
 ##---------------------------------------------------------
