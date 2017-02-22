5a8ea6511ed53488f2b57228d3fb1ec4b41405d2
Thomas Yu <thomasyu888@gmail.com>
2017-02-22 15:22:44 -0800

    Make sure metadatapath is correct

--- scoring_harness/challenge.py
+++ scoring_harness/challenge.py
@@ -179,7 +179,7 @@ def validate(evaluation, canCancel, dry_run=False):
     sys.stdout.flush()
     team_mapping_table = syn.tableQuery('select * from syn8220615')
     team_mapping = team_mapping_table.asDataFrame()
-    metadataPath = syn.get("syn8290709").path
+    metadataPath = syn.get("syn8303410").path
     metadata = pd.read_excel(metadataPath)
     patientIds = set(metadata.patientId)
     
