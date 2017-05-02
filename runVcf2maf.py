ee0bd670a7914a63a23631184c51e9d06ee1413f
Thomas Yu <thomasyu888@gmail.com>
2017-05-01 18:01:32 -0700

    vcf2maf ran on samples

---
+++ runVcf2maf.py
@@ -0,0 +1,71 @@
+
+import os
+import pandas as pd
+# def vcftomaf(vcffiles, path_to_GENIE, center,
+# 			 vcf2mafPath = "/home/tyu/software/vcf2maf",
+# 			 veppath="/home/tyu/software/vep", 
+# 			 vepdata="/home/tyu/.vep", 
+# 			 reference="/home/tyu/reference/hg19/hg_19_all_chrs.fasta"):
+vcf2mafPath = "/home/ubuntu/vcf2maf-1.6.12"
+veppath = "/home/ubuntu/vep"
+vepdata = "/home/ubuntu/.vep"
+reference = "/home/ubuntu/.vep/homo_sapiens/86_GRCh38/Homo_sapiens.GRCh38.dna.primary_assembly.fa.gz"
+tesla_path = "/home/ubuntu/tesla/TESLA_VCF"
+modifiedVCFPath = "/home/ubuntu/tesla"
+ncbibuild = "GRCh38"
+#centerInputFolder = os.path.join(path_to_GENIE,center,"input")
+#centerStagingFolder = os.path.join(path_to_GENIE,center,"staging")
+mafFiles = []
+files = os.walk(tesla_path)
+vcffiles = []
+for dirpath, dirname, filenames in files:
+	vcffiles.extend([os.path.join(dirpath,filename) for filename in filenames if filename.endswith(".vcf")])
+
+for path in vcffiles:
+	vcfName = os.path.basename(path)
+	metadata = vcfName.split("_")
+	team = metadata[0]
+	sample = metadata[1]
+	newVCFPath = os.path.join(modifiedVCFPath, vcfName)
+	os.system("sed 's/^chr//' %s > %s" % (path, newVCFPath))
+	os.system("sed -i 's/\t\t/\t.\t/g' %s" % newVCFPath)
+	os.system("sed -i 's/ p\./,p./' %s" % newVCFPath)
+	# vcfCols = ["#CHROM","POS","ID","REF","ALT","QUAL","FILTER","INFO","FORMAT"]
+	# with open(newVCFPath,"r") as f:
+	# 	for line in f:
+	# 		if line.startswith("#CHROM"):
+	# 			cols = line
+	# cols = cols.replace("\n","")
+	# cols = cols.replace("\r","")
+	# cols = cols.split("\t")
+
+	if os.path.isfile(newVCFPath+".maf"):
+		mafFiles.append(newVCFPath+".maf")
+	else:
+		os.system(('perl %s/vcf2maf.pl '
+				   '--input-vcf %s ' 
+				   '--output-maf %s.maf '
+				   '--vep-path %s ' 
+				   '--vep-data %s '
+				   '--vep-forks 8 '
+				   '--tumor-id %s '
+				   '--maf-center %s '
+				   '--custom-enst %s/data/isoform_overrides_uniprot '
+				   '--ncbi-build %s '
+				   '--ref-fasta %s') % (vcf2mafPath, newVCFPath, newVCFPath, veppath, vepdata, sample, team, vcf2mafPath, ncbibuild, reference))
+		if (os.path.isfile(newVCFPath+".maf")):
+			mafFiles.append(newVCFPath + ".maf")
+
+maf = pd.DataFrame()
+for i in mafFiles:
+	# mafName = os.path.basename(i)
+	# metadata = mafName.split("_")
+	# team = metadata[0]
+	temp = pd.read_csv(i,sep="\t",comment="#")
+	if len(temp)>0:
+		#temp['Center'] = team
+		maf = maf.append(temp)
+maf.to_csv("TESLA_uncleaned_vcf.maf",sep="\t",index=False)
+syn.store(synapseclient.File("TESLA_uncleaned_vcf.maf",parentId = "syn8123644"))
+syn.store(synapseclient.File("uncleanedVCF_vcf2maf_logs.txt",parentId = "syn8123644"))
+
